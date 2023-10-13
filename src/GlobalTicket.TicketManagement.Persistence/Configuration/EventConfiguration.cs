a09af07ffc5eb3b52b4cab1d48cc0b2af4380040
LourenceQ <lawrenceqf@gmail.com>
2023-10-12 21:38:42 -0300

    Implementando base repository

---
+++ src/GlobalTicket.TicketManagement.Persistence/Configuration/EventConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GlobalTicket.TicketManagement.Persistence.Configuration;
+public class EventConfiguration : IEntityTypeConfiguration<Event>
+{
+    public void Configure(EntityTypeBuilder<Event> builder)
+    {
+        builder.Property(e => e.Name).IsRequired().HasMaxLength(50);
+    }
+}
