392d0b8d3c511bbfee0bb4b1ee62f1fe130a685a
LourenceQ <lawrenceqf@gmail.com>
2023-10-17 21:21:32 -0300

    Implementando application layer

---
+++ src/GlobalTicket.TicketManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using GlobalTicket.CelanArch.Application.Contracts.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GlobalTicket.TicketManagement.Infrastructure;
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services
+        , IConfiguration config)
+    {
+        services.Configure<EmailSettings>(config.GetSection("EmailSettings"));
+
+        services.AddTransient<IEmailService, EmailService>();
+
+        return services;
+    }
+}
---
+++ src/GlobalTicket.TicketManagement.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using GlobalTicket.CelanArch.Application.Contracts.Infrastructure;
+using GlobalTicket.CelanArch.Application.Models.Mail;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace GlobalTicket.TicketManagement.Infrastructure.Mail;
+public class EmailService : IEmailService
+{
+    public EmailSettings _emailSettings { get; }
+
+    public EmailService(EmailSettings emailSettings)
+    {
+        _emailSettings = emailSettings;
+    }
+
+    public Task<bool> SendEmail(Email email)
+    {
+
+        var client = new SendGridClient(_emailSettings.ApiKey);
+
+        var subject = email.Subject;
+        var to = new EmailAddress(email.To);
+        var emailBody = email.Body;
+
+        var from = new EmailAddress
+        {
+            Email = _emailSettings.FromAddress,
+            Name = _emailSettings.FromName
+        };
+
+        var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+        var response = await client.SendEmailAsync(sendGridMessage);
+
+
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+            return true;
+
+        return false;
+    }
+}
