7a22d3f6f9661655860cb4f58fcde9ce1d58bf0d
LourenceQ <lawrenceqf@gmail.com>
2023-10-30 21:14:34 -0300

    implementando teste base para integração de api

---
+++ src/GloboTicket.TicketManagement.API.IntegrationTests/Categories/Queries/GetCategoriesListQueryHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using GlobalTicket.CelanArch.Application.Contracts.Persistance;
+using GlobalTicket.CelanArch.Application.Features.Categories.Queries.GetCategoriesList;
+using GlobalTicket.CelanArch.Application.Profiles;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Moq;
+using Shouldly;
+
+namespace GloboTicket.TicketManagement.API.IntegrationTests.Categories.Queries;
+public class GetCategoriesListQueryHandlerTests
+{
+    private readonly IMapper _mapper;
+    private readonly Mock<IAsyncRepository<Category>> _mockCategoryRepository;
+
+    public GetCategoriesListQueryHandlerTests()
+    {
+        _mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+        var configurationProvider = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<MappingProfile>();
+        });
+
+        _mapper = configurationProvider.CreateMapper();
+    }
+
+    [Fact]
+    public async Task GetCategoriesListTest()
+    {
+        var handler = new GetCategoriesListQueryHandler(_mockCategoryRepository.Object, _mapper);
+
+        var result = await handler.Handle(new GetCategoriesListQuery(),
+            CancellationToken.None);
+
+        result.ShouldBeOfType<List<CategoryListVm>>();
+    }
+
+}
