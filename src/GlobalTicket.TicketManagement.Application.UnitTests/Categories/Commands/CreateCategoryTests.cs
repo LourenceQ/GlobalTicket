78191340f210edb342ea816fdcba760a882d325f
LourenceQ <lawrenceqf@gmail.com>
2023-11-07 18:29:12 -0300

    implementando testes em application para CreateCategoriesListQueryHandler

---
+++ src/GlobalTicket.TicketManagement.Application.UnitTests/Categories/Commands/CreateCategoryTests.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using GlobalTicket.CelanArch.Application.Contracts.Persistance;
+using GlobalTicket.CelanArch.Application.Profiles;
+using GlobalTicket.TicketManagement.Application.Features.Categories.Commands.CreateCateogry;
+using GlobalTicket.TicketManagement.Application.UnitTests.Mocks;
+using GlobalTicket.TicketManagement.Domain.Entities;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace GlobalTicket.TicketManagement.Application.UnitTests.Categories.Commands;
+public class CreateCategoryTests
+{
+    private readonly IMapper _mapper;
+    private readonly Mock<IAsyncRepository<Category>> _mockCategoryRepository;
+
+    public CreateCategoryTests()
+    {
+        _mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+        var configurationProvider = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<MappingProfile>();
+        });
+
+        _mapper = configurationProvider.CreateMapper();
+    }
+
+    [Fact]
+    public async Task Handle_ValidCategory_AddedToCategoriesRepo()
+    {
+        var handler = new CreateCategoryCommandHandler(_mapper, _mockCategoryRepository.Object);
+
+        await handler.Handle(new CreateCategoryCommand() { Name = "Test" }, CancellationToken.None);
+
+        var allCategories = await _mockCategoryRepository.Object.ListAllAsync();
+        allCategories.Count.ShouldBe(5);
+    }
+}
